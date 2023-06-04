25881ca35006109570b128e314524a1fa4eb1c3e
Jaidyn Ann <10477760+JadedCtrl@users.noreply.github.com>
2023-06-03 22:13:45 -0500

    Remove source-file copyright header

--- flora-search-aurora.lisp
+++ flora-search-aurora.lisp
@@ -1,23 +1,5 @@
-;;
-;; Copyright 2023, Jaidyn Ann <jadedctrl@posteo.at>
-;;
-;; This program is free software: you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation, either version 3 of
-;; the License, or (at your option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-;; GNU General Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License
-;; along with this program. If not, see <https://www.gnu.org/licenses/>.
-;;
-
-;; Made for the text-flavoured LibreJam of 2023-06!
-;; https://jamgaroo.xyz/jams/2
-;; 72x20
+;;;; A simple TUI-game made for the text-flavoured LibreJam of 2023-06!
+;;;; https://jamgaroo.xyz/jams/2
 
 (ql:quickload '(alexandria cl-tiled str))
 
